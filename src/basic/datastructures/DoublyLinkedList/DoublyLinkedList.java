dd5fc7b4f424c218117d820d6d1e2564e26a9074
saequus <slava@spetsyian.com>
2021-01-16 16:34:24 +0300

    [Feat] udated doubly linked list

--- src/basic/datastructures/DoublyLinkedList/DoublyLinkedList.java
+++ src/basic/datastructures/DoublyLinkedList/DoublyLinkedList.java
@@ -56,11 +56,64 @@ class Node {
 			this.next = null;
 		}
 	}
+
+	public void traverseForwards() {
+		System.out.println("Forwards traversion started: ");			
+		Node current = this;
+		int i = 0;
+		while (current != null) {
+			System.out.println("Node " + i + " value: " + current.value);
+			current = current.next;
+			i++;
+		}
+	}
+
+	public void traverseBackwards() {
+		System.out.println("Backwards traversion started: ");	
+
+		Node current = this;
+		int i = 0;
+		while (current != null) {
+		System.out.println("Node " + Math.abs(i) + " value: " + current.value);
+			current = current.prev;
+			i--;	
+		}
+	}
+}
+
+class Manage {
+	public static void rmDoublyLinkedListNodeByNode(Node startNode, boolean forwards) {
+		String direction = forwards == true ? "Forwards" : "Backwards";
+		System.out.println(direction + " deletion node by node started: ");
+		if (forwards == true) {
+			int i = 0;
+			Node current = startNode;
+			while (current != null) {
+				System.out.println("Removed node " + i + " with value: " + current.value);
+				Node prev = current;
+				current = current.next;
+				prev.next = null;
+				i++;
+			}
+		} else {
+			int i = 0;
+			Node current = startNode;
+			while (current != null) {
+				System.out.println("Removed node " + i + " with value: " + current.value);
+				Node next = current;
+				current = current.prev;
+				next.prev = null;
+				i--;
+			}
+		}
+	}
 }
 
 
 class Main {
 	public static void main(String[] args) {
+		System.out.print("Unseccussful doubly linked list creation with Exception called");
+
 		Node n1 = new Node(1);
 		Node n2 = new Node(2);
 		Node n3 = new Node(3);
@@ -68,15 +121,64 @@ class Main {
 		try {
 			n1.addNext(n2);
 			n2.addNext(n3);
-		}
-		catch (Exception e) {
-			System.out.print("Print");
-		}
-		try {
 			n2.addNext(n1);
 		} catch (Exception e) {
-			System.out.print("[" + e + "], error occured.");
+			System.out.print("Error occured: [" + e + "]");
 		}
 	}
 }
 
+class Main2 {
+	public static void main(String[] args) {
+
+		System.out.println("Successful doubly linked list creation and traverse forwards and backwards:");
+
+		Node n1 = new Node(1);
+		Node n2 = new Node(2);
+		Node n3 = new Node(3);
+		Node n4 = new Node(4);
+
+		try {
+			n1.addNext(n2);
+			n2.addNext(n3);
+			n3.addNext(n4);
+		} catch (Exception e) {
+			System.out.print("Error occured: [" + e + "]");
+		}
+
+		try {
+			n4.addPrev(n3);
+			n3.addPrev(n2);
+			n2.addPrev(n1);
+		} catch (Exception e) {
+			System.out.print("Error occured: [" + e + "]");
+		}	
+	
+
+		n1.traverseForwards();
+		n4.traverseBackwards();
+	}
+
+}
+
+class Main3 {
+	public static void main(String[] args) {
+		System.out.print("Successfull linked list creation and removal node by node");
+			
+		Node n1 = new Node(1);
+		Node n2 = new Node(2);
+		Node n3 = new Node(3);
+		Node n4 = new Node(4);
+		
+		try {
+			n1.addNext(n2);
+			n2.addNext(n3);
+			n3.addNext(n4);
+		} catch (Exception e) {
+			System.out.print("Error occured: [" + e + "]");
+		}	
+
+		Manage mn = new Manage();
+		mn.rmDoublyLinkedListNodeByNode(n1, true);	
+	}
+}
